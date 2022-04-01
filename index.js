508bc7566336e121c286ff7d9019a7019a5eabbd
Blair Bristow <blairhbristow@gmail.com>
2022-04-01 17:19:26 -0400

    Further standardization

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const mango = document.querySelector('#mangue');
 
 mango.addEventListener('click', function (event) {
-    mango.style.animation = 'spin'
-    mango.style.animationDuration = '1s'
+    mango.style.animation = 'spin';
+    mango.style.animationDuration = '1s';
     mango.style.animationIterationCount = 5;
-    mango.style.animationTimingFunction = 'linear'
+    mango.style.animationTimingFunction = 'linear';
 })
 
 mango.addEventListener('dblclick', function (event) {
     mango.style.position = 'relative';
     mango.style.left = '0px';
-    mango.style.left = '25px'
+    mango.style.left = '25px';
 })
